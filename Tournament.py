71177b7a2d13f515729553bd6d75c38f0f6e7040
Propar2750 <82144119+Propar2750@users.noreply.github.com>
2024-02-21 13:52:31 +0530

    Update and rename Version_1.py to Tournament.py
    --> Added 3 more players
    --. Improved the format and output

--- Tournament.py
+++ Tournament.py
@@ -1,6 +1,7 @@
 import random
+import pandas as pd
 
-
+# The main function in which player 1 and player 2 complete
 def main(player_1, player_2):
     player_1_moves = []
     player_2_moves = []
@@ -21,33 +22,39 @@ def main(player_1, player_2):
         elif player_1_move == "cooperating" and player_2_move == "cooperating":
             score_1 += 3
             score_2 += 3
+        # print(f"{(player_1.__name__)} did {player_1_move} and {player_2.__name__} did {player_2_move}")
     print(f"{(player_1.__name__)} scored {score_1} and {player_2.__name__} scored {score_2}")
     final_scores[player_1.__name__] = final_scores[player_1.__name__] + score_1
     final_scores[player_2.__name__] = final_scores[player_2.__name__] + score_2
 
 
+# ------- Participants --------
 def tit_for_tat(previous_moves):
+    # Does whatever the opponent did in the previous move
     if len(previous_moves) == 0:
         return "cooperating"
     else:
-        return previous_moves[len(previous_moves) - 1]
+        return previous_moves[-1]
 
 
 def generous_tit_for_tat(previous_moves):
+    # There is a 10% chance to be co-operating otherwise it does whatever the opponent did in the previous move
     x = random.randint(0, 7)
     if len(previous_moves) == 0:
         return "cooperating"
     elif x == 1:
         return "cooperating"
     else:
-        return previous_moves[len(previous_moves) - 1]
+        return previous_moves[-1]
 
 
 def always_defect(previous_moves):
+    # Always defects
     return "defect"
 
 
 def always_cooperate(previous_moves):
+    # Always cooperates
     return "cooperating"
 
 
@@ -60,14 +67,14 @@ def friedman(previous_moves):
 
 
 def joss(previous_moves):
-    # Sneaky
+    # Sneaky (10% Chance to randomly deflect) otherwise it does whatever the opponent did in the previous move
     x = random.randint(0, 9)
     if x == 0:
         return "defect"
     elif len(previous_moves) == 0:
         return "cooperating"
     else:
-        return previous_moves[len(previous_moves) - 1]
+        return previous_moves[-1]
 
 
 def tester_1(previous_moves):
@@ -92,18 +99,22 @@ def tester_1(previous_moves):
 
 
 def two_tits_for_tat(previous_moves):
+    # Defects Twice if opponent defected once
     if len(previous_moves) == 0:
         return "cooperating"
-    elif previous_moves[len(previous_moves) - 1] == "defect" or previous_moves[len(previous_moves) - 2] == "defect":
+    elif previous_moves[-1] == "defect":
+        return "defect"
+    elif len(previous_moves) >= 2 and previous_moves[-2] == "defect":
         return "defect"
     else:
         return "cooperating"
 
 
 def tit_for_two_tats(previous_moves):
-    if len(previous_moves) == 0:
+    # Defects once if opponent defected twice
+    if len(previous_moves) <= 1:
         return "cooperating"
-    elif previous_moves[len(previous_moves) - 1] == "defect" and previous_moves[len(previous_moves) - 2] == "defect":
+    elif previous_moves[-1] == "defect" and previous_moves[-2] == "defect":
         return "defect"
     else:
         return "cooperating"
@@ -117,18 +128,74 @@ def fun_random(previous_moves):
         return "cooperating"
 
 
-list_of_players = [tit_for_tat, always_defect, friedman, joss, tester_1, generous_tit_for_tat, always_cooperate,
-                   two_tits_for_tat, tit_for_two_tats, fun_random]
+def ashwika_1(previous_moves):
+    # two tit for two tat, two cooperating means forgiveness
+    store = "cooperating"
+    if len(previous_moves) == 0:
+        return "cooperating"
+    elif "defect" in previous_moves:
 
+        no = 0
+        for i in range(len(previous_moves) - 1, -1, -1):
+
+            if previous_moves[i - 1] == "defect" and previous_moves[i] == "defect":
+                store = "defect"
+                no = i
+                break
+        for i in range(len(previous_moves) - 1, no, -1):
+            if previous_moves[i] == "cooperating" and previous_moves[i - 1] == "cooperating":
+                store = "cooperating"
+                break
+    return store
+
+
+def nasty_forgiving(previous_moves):
+    # Nasty, forgiving, Retaliatory
+    x = random.randint(0, 6)
+    if x == 1:
+        return "defect"
+    elif x == 2:
+        return "cooperating"
+    elif len(previous_moves) == 0:
+        return "cooperating"
+    else:
+        return previous_moves[-1]
+
+
+def nasty_not_forgiving(previous_moves):
+    # Nasty, forgiving, Retaliatory
+    x = random.randint(0, 6)
+    if x == 1:
+        return "defect"
+
+    elif "defect" in previous_moves:
+        return "defect"
+
+    elif x == 2:
+        return "cooperating"
+
+    else:
+        return "cooperating"
+# --------- Participants End ---------
+
+# Basic Setup Starts
+list_of_players = [tit_for_tat, always_defect, friedman, joss, tester_1, generous_tit_for_tat, always_cooperate,
+                   two_tits_for_tat, tit_for_two_tats, fun_random, ashwika_1, nasty_forgiving, nasty_not_forgiving]
+population = {}
 final_scores = {}
 for i in range(0, len(list_of_players)):
     final_scores[list_of_players[i].__name__] = 0
+# Basic Setup Ends
 
-for k in range(1,6):
+# A tournament of 200 rounds
+def tournamnent():
+    global final_scores
+
     for i in range(0, len(list_of_players)):
         for j in range(0, len(list_of_players)):
 
             main(list_of_players[i], list_of_players[j])
-
-final_scores = dict(sorted(final_scores.items(), key=lambda x: x[1], reverse=True))
-print(final_scores)
+    final_scores = dict(sorted(final_scores.items(), key=lambda x: x[1], reverse=True))
+    print(final_scores)
+tournamnent()
+df = pd.DataFrame(data=final_scores)
