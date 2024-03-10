6251c883e3ae78d132d2a3110964c8231679749c
Propar2750 <82144119+Propar2750@users.noreply.github.com>
2024-03-10 11:13:28 +0530

    Update evolution.py
    --> improved formatting

--- evolution.py
+++ evolution.py
@@ -1,16 +1,21 @@
 import copy
 import random
-from participants import always_defect,always_cooperate,ashwika_1,nasty_forgiving,nasty_not_forgiving,fun_random,friedman,tit_for_two_tats,two_tits_for_tat,generous_tit_for_tat,tit_for_tat,joss,tester_1,list_of_players
+
 import pandas as pd
+
+from participants import always_defect, ashwika_1, nasty_forgiving, friedman, tit_for_tat, joss, list_of_players
+
 # Toggles:
-noise = True # If y ou want a rarandom chance that the actions are misinterdpreted
+noise = True  # If you want a random chance that the actions are misinterpreted
 noise_intensity = 40  # 1 in (Noise_intensity) chance to do opposite
-print_result_of_each_match = False # Activates a print statment that prints the result of each match
+print_result_of_each_match = False  # Activates a print statement that prints the result of each match
 do_evolution = True
-intensity = False 
+intensity = False
 # Main input
-population = {tit_for_tat: 10, always_defect: 10,joss:10,ashwika_1:10,nasty_forgiving:10,friedman:10} # The main population dictinary which stores the current populations
-num_of_evolutions = 10 # Number of evolutions you want to do 
+population = {tit_for_tat: 10, always_defect: 10, joss: 10, ashwika_1: 10, nasty_forgiving: 10,
+              friedman: 10}  # The main population dictionary which stores the current populations
+num_of_evolutions = 10  # Number of evolutions you want to do
+
 
 # The main function in which player 1 and player 2 complete
 def main(player_1, player_2):
@@ -19,10 +24,9 @@ def main(player_1, player_2):
     score_1 = 0
     score_2 = 0
 
-    for i in range(1, 201):
+    for round_number in range(1, 201):
         player_1_move = player_1(player_2_moves)
         player_2_move = player_2(player_1_moves)
-
 
         if noise:  # If noise == True
             x = random.randint(0, noise_intensity - 1)
@@ -57,13 +61,10 @@ def main(player_1, player_2):
     final_scores[player_2.__name__] = final_scores[player_2.__name__] + score_2
 
 
-
-
-
 # Basic Setup Starts
 
 
-population_list = [] # K
+population_list = []  # K
 total_people = 0
 
 population_print = {x.__name__: population[x] for x in population}
@@ -81,12 +82,12 @@ for i in range(0, len(list_of_players)):
 # Basic Setup Ends
 
 # A tournament of 200 rounds
-def tournamnent():
+def tournament():
     global final_scores
 
-    for i in range(0, len(list_of_participants)):
-        for j in range(0, len(list_of_participants)):
-            main(list_of_participants[i], list_of_participants[j])
+    for participant_1 in list_of_participants:
+        for participant_2 in list_of_participants:
+            main(participant_1, participant_2)
     final_scores = dict(sorted(final_scores.items(), key=lambda x: x[1], reverse=True))
 
 
@@ -124,49 +125,51 @@ def evolution_1():
     for i in range(0, len(list_of_players)):
         final_scores[list_of_players[i].__name__] = 0
 
+
 # Making the growing population dependent on a higher ratio of score per capita as a percentage of all score per capita
 def evolution_2():
     global final_scores, total_people
 
     # Appending the current population to the populations list which will be accessed later for the final output
     population_list.append(copy.deepcopy(population_print))
-    
+
     # Defining a new dictionary {player_function: total income of player_function.__name__/population of player_function
-    population_per_capita = {x: final_scores[x.__name__] / population[x] for x in list(population.keys()) if population[x]!= 0}
-    
+    population_per_capita = {x: final_scores[x.__name__] / population[x] for x in list(population.keys()) if
+                             population[x] != 0}
+
     # Defining a new variable total score which will be the additional of all per capita average incomes
     total_score = 0
     for i in population_per_capita.values():
         total_score += i
-        
+
+    # Defining a new variable since we cant change the total amount of people before the entire code has run so it 
+    # will be changed after 
     total_population_change = 0
-    
-    
-    population_per_capita_ratio = {key:value/total_score for key,value in (population_per_capita).items()}
-    for key,value in (population_per_capita_ratio).items():
-        final_p = population[key] + round((value-(1/len(population.keys()))+0.005)*total_people)
+
+    population_per_capita_ratio = {key: value / total_score for key, value in (population_per_capita).items()}
+    for key, value in population_per_capita_ratio.items():
+        final_p = population[key] + round((value - (1 / len(population.keys())) + 0.005) * total_people)
         total_population_change = final_p - population[key]
 
-
-        if population[key]<0:
+        if population[key] < 0:
             population.pop(key)
-        elif final_p>population[key]:
-            for i in range(0, final_p-population[key]):
+        elif final_p > population[key]:
+            for i in range(0, final_p - population[key]):
                 list_of_participants.append(key)
-        elif final_p < 0 :
-            for i in range(0,population[key]):
+        elif final_p < 0:
+            for i in range(0, population[key]):
                 list_of_participants.remove(key)
             population.pop(key)
-        elif final_p<population[key]:
+        elif final_p < population[key]:
             for i in range(0, population[key] - final_p):
                 try:
                     list_of_participants.remove(key)
                 except ValueError:
-                    print("Value Error",final_p,population[key],key)
+                    print("Value Error", final_p, population[key], key)
         population[key] = final_p
         population_print[key.__name__] = final_p
         print(len(list_of_participants))
-        #print(key.__name__,final_p,value,value*5*(1-value)*population[key])
+        # print(key.__name__,final_p,value,value*5*(1-value)*population[key])
 
     final_scores = {}
     for i in range(0, len(list_of_players)):
@@ -176,7 +179,7 @@ def evolution_2():
 
 if do_evolution:
     for i in range(0, num_of_evolutions):
-        tournamnent()
+        tournament()
         evolution_2()
 population_plot = {x.__name__: [] for x in list_of_players}
 for dict in population_list:
@@ -187,5 +190,5 @@ for dict in population_list:
             population_plot[key].append(0)
 df = pd.DataFrame(data=population_plot)
 print(df)
-df.to_excel("population.xlsx",index=False)
+df.to_excel("population.xlsx", index=False)
 print(population_plot)
